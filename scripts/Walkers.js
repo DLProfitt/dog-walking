87da0a40bfcc04140c20bcdd2112e8d1192afcb9
David Profitt <dlprofitt@gmail.com>
2023-03-08 08:28:36 -0500

    Book-3_Ch-8 Completed, assignments still ends in 'undefined'

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,34 +1,37 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities, getWalkerCities } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click", 
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            const [,walkerId] = itemClicked.id.split("--")
+            const walkerCities = getWalkerCities().filter(wc => wc.walkerId === parseInt(walkerId))
+            const cityNames = getCityNames(walkerCities)
+            window.alert(`${itemClicked.textContent.trim()} services ${cityNames}`)
+        }
+    }
+)
+
+const getCityNames = (walkerCities) => {
+    const cities = getCities()
+    const cityNames = []
+    for (const wc of walkerCities) {
+        const city = cities.find(c => c.id === wc.cityId)
+        if (city) {
+            cityNames.push(city.name)
+        }
+    }
+    return cityNames.join(", ")
+}
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
-
     for (const walker of walkers) {
         walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
     }
-
     walkerHTML += "</ul>"
-
-    // Add event listener to list items with IDs that start with "walker"
-    document.addEventListener(
-        "click",
-        (clickEvent) => {
-            const itemClicked = clickEvent.target
-
-            if (itemClicked.id.startsWith("walker")) {
-                const [, walkerId] = itemClicked.id.split("--")
-
-                for (const walkerObject of walkers) {
-                    if (walkerObject.id === parseInt(walkerId)) {
-                        window.alert(`${walkerObject.name} services ${walkerObject.city}`)
-                    }
-                }
-            }
-        }
-    )
-
     return walkerHTML
 }
